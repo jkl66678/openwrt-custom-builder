88183e5426e9154a5dcdc4863e3305ccefaa125d
jkl66678 <3500523467@qq.com>
2025-07-29 18:21:58 +0800

    Update custom-init.sh

--- scripts/custom-init.sh
+++ scripts/custom-init.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-# 自定义初始化脚本：修改默认设置，在编译前执行
+# 编译前自定义配置
 
-# 1. 修改默认管理IP（从192.168.1.1改为192.168.5.1）
+# 1. 修改默认管理IP（192.168.1.1 → 192.168.5.1）
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
-# 2. 添加自定义防火墙规则（开放8080端口）
+# 2. 开放自定义端口（8080）
 echo 'iptables -A INPUT -p tcp --dport 8080 -j ACCEPT' >> package/base-files/files/etc/rc.local
 
-# 3. 修改默认主机名（如改为"MyRouter"）
+# 3. 修改默认主机名
 sed -i 's/OpenWrt/MyRouter/g' package/base-files/files/bin/config_generate
 
-# 4. 禁用首次登录强制修改密码（可选）
-sed -i 's/exit 0//g' package/base-files/files/etc/rc.local
+# 4. 禁用首次登录强制改密码
+sed -i '/exit 0/d' package/base-files/files/etc/rc.local
 echo 'uci set luci.main.mediaurlbase=/luci-static/bootstrap' >> package/base-files/files/etc/rc.local
 echo 'uci commit luci' >> package/base-files/files/etc/rc.local
 echo 'exit 0' >> package/base-files/files/etc/rc.local
-    
+
+# 5. 调整时区为上海
+sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
